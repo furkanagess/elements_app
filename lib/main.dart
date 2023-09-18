fee7fc33949f27c82a4438a1766af429019eb2c9
Furkan Caglar <caglarrfurkann@gmail.com>
2023-09-18 21:56:00 +0300

    appname changed

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Material App',
+      title: 'Elements App',
       home: ElementsListScreen(),
     );
   }
