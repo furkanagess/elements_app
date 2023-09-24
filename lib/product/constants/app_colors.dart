0b5ff6746688e5042599027b86c3586e2f5c49c6
Furkan Caglar <caglarrfurkann@gmail.com>
2023-09-24 22:19:47 +0300

    constants fixes

---
+++ lib/product/constants/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  const AppColors._();
+  // Standart Colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color background = Color(0xff0e0e14);
+  static const Color darkBlue = Color(0xff13132c);
+  static const Color transparentWhite = Color(0xff353535);
+  static const Color lightWhite = Color(0xffadadad);
+  // Element Colors
+  static const Color lightPurple = Color(0xFFB8B8FF);
+  static const Color pistachioGreen = Color(0xFFD7FFCE);
+  static const Color turqiouse = Color(0xFFBEF1F6);
+  static const Color snuff = Color(0xFFDECCE5);
+  static const Color lightPink = Color(0xFFFFDBDB);
+  static const Color lightYellow = Color(0xFFFCF6BD);
+  static const Color banana = Color(0xFFFCEAA8);
+
+  // new colors
+  static const Color skinColor = Color(0xFFEF9595);
+  static const Color purple = Color(0xffBEADFA);
+  static const Color lightTurq = Color(0xff96B6C5);
+}
