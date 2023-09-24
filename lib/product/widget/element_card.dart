54a9bb3c84e775bb612af2a5d404217885d75a5a
Furkan Caglar <caglarrfurkann@gmail.com>
2023-09-24 11:38:33 +0300

    widget fixes

--- lib/product/widget/element_card.dart
+++ lib/product/widget/element_card.dart
@@ -1,9 +1,8 @@
-import 'package:elements_app/feature/elements/element_detail_screen.dart';
-import 'package:elements_app/product/model/table_element.dart';
+import 'package:elements_app/product/model/periodic_element.dart';
 import 'package:flutter/material.dart';
 
 class ElementCard extends StatelessWidget {
-  final TableElement element;
+  final PeriodicElement element;
 
   const ElementCard({
     super.key,
@@ -13,19 +12,12 @@ class ElementCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(8.0),
+      margin: EdgeInsets.all(8.0),
       child: ListTile(
         title: Text('${element.number}. ${element.name} (${element.symbol})'),
         subtitle:
             Text('Category: ${element.category}, Block: ${element.block}'),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ElementDetailScreen(element: element),
-            ),
-          );
-        },
+        // Diğer element özelliklerini buraya ekleyebilirsiniz
       ),
     );
   }
