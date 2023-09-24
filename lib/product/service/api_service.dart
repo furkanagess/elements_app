b7d3b7823545bf10d8a42669e2d3862b01047f35
Furkan Caglar <caglarrfurkann@gmail.com>
2023-09-24 22:19:09 +0300

    general fixes

--- lib/product/service/api_service.dart
+++ lib/product/service/api_service.dart
@@ -1,13 +1,10 @@
-import 'package:elements_app/product/model/periodic_element.dart';
+import 'package:elements_app/feature/model/periodic_element.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  static const String apiUrl =
-      'https://raw.githubusercontent.com/furkanagess/periodic_table_data_set/master/all_elements.json'; // API endpointini buraya ekleyin
-
-  Future<List<PeriodicElement>> fetchElements() async {
-    final response = await http.get(Uri.parse(apiUrl));
+  Future<List<PeriodicElement>> fetchElements(String apiType) async {
+    final response = await http.get(Uri.parse(apiType));
 
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = json.decode(response.body);
