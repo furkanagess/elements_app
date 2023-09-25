4992e63c702a4ca9305c96c19b373cf1df731d26
Furkan Caglar <caglarrfurkann@gmail.com>
2023-09-25 16:10:49 +0300

    color_extension added

---
+++ lib/product/extensions/color_extension.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+extension ColorExtension on String {
+  toColor() {
+    var hexStringColor = this;
+    final buffer = StringBuffer();
+
+    if (hexStringColor.length == 6 || hexStringColor.length == 7) {
+      buffer.write('ff');
+      buffer.write(hexStringColor.replaceFirst("#", ""));
+      return Color(int.parse(buffer.toString(), radix: 16));
+    }
+  }
+}
