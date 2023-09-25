0856a840f840aef0ac1c99ac008fe1058dee00b8
Furkan Caglar <caglarrfurkann@gmail.com>
2023-09-25 16:11:12 +0300

    service relocated

---
+++ lib/feature/service/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:elements_app/feature/model/periodic_element.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiService {
+  Future<List<PeriodicElement>> fetchElements(String apiType) async {
+    final response = await http.get(Uri.parse(apiType));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((e) => PeriodicElement.fromJson(e)).toList();
+    } else {
+      throw Exception('API isteği başarısız oldu: ${response.statusCode}');
+    }
+  }
+}
