cecb948692af6442c856bf2b643763fad54d7492
Furkan Caglar <caglarrfurkann@gmail.com>
2023-09-25 17:23:26 +0300

    app_strings fixes

--- lib/product/constants/app_strings.dart
+++ lib/product/constants/app_strings.dart
@@ -3,5 +3,19 @@ import 'package:flutter/material.dart';
 @immutable
 class AppStrings {
   const AppStrings._();
+  // General
   static const String appName = 'Elementler';
+  static const String errorMsg = 'Bir hata oluştu';
+  // All Elements
+  static const String allElements = 'Tüm Elementler';
+  // Element Detail
+  static const String block = 'Blok';
+  static const String period = 'Periyot';
+  static const String group = 'Grup';
+  static const String name = 'İsim:';
+  static const String number = 'Numara:';
+  static const String weight = 'Ağırlık:';
+  static const String description = 'Tanım';
+  static const String usage = 'Kullanım Alanı';
+  static const String source = 'Kaynak';
 }
