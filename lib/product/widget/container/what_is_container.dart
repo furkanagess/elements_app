5d61570ecc8c82bf2870a8efea265b6fe9b2330d
Furkan Caglar <caglarrfurkann@gmail.com>
2023-10-13 16:59:04 +0300

    widget fixes

--- lib/product/widget/container/what_is_container.dart
+++ lib/product/widget/container/what_is_container.dart
@@ -1,64 +1,71 @@
+import 'package:elements_app/feature/model/info.dart';
+import 'package:elements_app/feature/view/home/info/subInfo/infoDetail/view/info_detail_view.dart';
 import 'package:elements_app/product/constants/app_colors.dart';
-import 'package:elements_app/product/constants/assets_constants.dart';
 import 'package:elements_app/product/extensions/context_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:elements_app/product/extensions/color_extension.dart';
 
 @immutable
 final class WhatIsContainer extends StatelessWidget {
-  final VoidCallback? onTap;
-  final String title;
-  final Color color;
-  final Color shColor;
+  final Info info;
+
   const WhatIsContainer({
     super.key,
-    required this.title,
-    required this.color,
-    required this.shColor,
-    required this.onTap,
+    required this.info,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
-      child: Container(
-        height: context.dynamicHeight(0.075),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: shColor,
-              offset: const Offset(4, 4),
-              spreadRadius: 1,
-            ),
-          ],
-          color: color,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Padding(
-          padding: context.paddingLowHorizontal,
-          child: Row(
-            children: [
-              SvgPicture.asset(
-                AssetConstants.instance.svgQuestionTwo,
-                height: context.dynamicHeight(0.05),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => InfoDetailView(info: info),
+          ),
+        );
+      },
+      child: Padding(
+        padding: context.paddingNormal,
+        child: Container(
+          height: context.dynamicHeight(0.075),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: info.shColor?.toColor(),
+                offset: const Offset(4, 4),
+                spreadRadius: 1,
               ),
-              SizedBox(
-                width: context.dynamicWidth(0.02),
-              ),
-              Text(
-                title,
-                style: context.textTheme.headlineSmall?.copyWith(
-                  color: AppColors.background,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Spacer(),
-              const Icon(
-                Icons.arrow_right_alt_outlined,
-                size: 40,
-              )
             ],
+            color: info.colors?.toColor(),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: context.paddingLowHorizontal,
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/svg/question-2.svg",
+                  height: context.dynamicHeight(0.05),
+                ),
+                SizedBox(
+                  width: context.dynamicWidth(0.03),
+                ),
+                Text(
+                  info.title!,
+                  style: context.textTheme.headlineSmall?.copyWith(
+                    color: AppColors.background,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+                const Icon(
+                  Icons.arrow_right_alt_outlined,
+                  size: 40,
+                )
+              ],
+            ),
           ),
         ),
       ),
