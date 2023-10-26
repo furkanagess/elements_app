1612dda2971a1f44bd0f761cc829460f57b3a86f
Furkan Caglar <caglarrfurkann@gmail.com>
2023-10-26 19:53:26 +0300

    google_ads_service fixes

--- lib/feature/service/google_ads_service.dart
+++ lib/feature/service/google_ads_service.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class GoogleAdsService {
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3499593115543692/9910553185";
+      return "ca-app-pub-3499593115543692/7181453654";
     } else {
       throw UnsupportedError("Unsupported platform");
     }
