ce92392185bd74a05b6c96bbdb5e6875d41ecfbe
Furkan Caglar <caglarrfurkann@gmail.com>
2023-11-02 19:24:16 +0300

    generated_plugin_registrant

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
-import firebase_core
-import firebase_messaging
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
 }
